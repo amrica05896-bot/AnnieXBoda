c78ebeb3990a51604d5fe6444e0c6bd29ad29272
q058961234-web <q058961234@gmail.com>
2026-02-01 20:21:21 +0200

    Update thumbnails.py

--- AnnieXMedia/utils/thumbnails.py
+++ AnnieXMedia/utils/thumbnails.py
@@ -7,7 +7,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 
 # ✅ التعديل هنا: استخدام __future__ بدل aio
-from pytubefix import Search
+from youtubesearchpython.__future__ import VideosSearch
 
 from config import YOUTUBE_IMG_URL
 from AnnieXMedia.core.dir import CACHE_DIR 
