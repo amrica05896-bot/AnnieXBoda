507dbc116b5fab51b0819f313c7793fce04066c8
q058961234-web <q058961234@gmail.com>
2026-02-01 20:19:12 +0200

    Update Spotify.py

--- AnnieXMedia/platforms/Spotify.py
+++ AnnieXMedia/platforms/Spotify.py
@@ -3,7 +3,7 @@ import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from pytubefix import Search
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 
