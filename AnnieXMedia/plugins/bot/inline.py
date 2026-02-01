632ae36e2e43965dd49f5e13e59917052e0a4d1b
q058961234-web <q058961234@gmail.com>
2026-02-02 01:10:54 +0200

    Update inline.py

--- AnnieXMedia/plugins/bot/inline.py
+++ AnnieXMedia/plugins/bot/inline.py
@@ -6,7 +6,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineQueryResultPhoto,
 )
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 from AnnieXMedia.utils.inlinequery import answer
 from config import BANNED_USERS
