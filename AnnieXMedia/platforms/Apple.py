5594f9aa7c82f9ec893531d3635a213ee6b706bd
q058961234-web <q058961234@gmail.com>
2026-02-01 20:18:39 +0200

    Update Apple.py

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -8,7 +8,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-from pytubefix import Search
+from youtubesearchpython.__future__ import VideosSearch
 
 class AppleAPI:
     def __init__(self):
