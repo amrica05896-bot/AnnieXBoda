116662da9c4703e1b3fd03deabbf1ac074c5e227
q058961234-web <q058961234@gmail.com>
2026-02-03 02:17:33 +0200

    Update __init__.py

--- pytgcalls/__init__.py
+++ pytgcalls/__init__.py
@@ -2,9 +2,8 @@ from .__version__ import __version__
 from .custom_api import CustomApi
 from .media_devices import MediaDevices
 from .pytgcalls import PyTgCalls
-# استيراد النسخ غير المتزامنة (Async) مباشرة بدل الـ Sync
-from .methods.utilities import compose
-from .methods.utilities import idle
+from .sync import compose
+from .sync import idle
 
 __all__ = (
     '__version__',
