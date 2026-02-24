aabf440b111de5477cb4763e594cb51e6c223073
b07849185-lang <b07849185@gmail.com>
2026-02-25 01:01:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,15 @@
+يكون كمبيوتر اول مخش الموقع 
 #!/bin/bash
 
 # 1. تنظيف ملفات الكاش القديمة عشان لو السيرفر عمل ريستارت الواجهة ما تعلقش
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1 /tmp/.X0-lock /tmp/.X11-unix/X0
 
-# 💡 التريكة: تحويل صفحة الـ VNC لتكون هي الواجهة الرئيسية للموقع مباشرة!
-cp /usr/share/novnc/vnc.html /usr/share/novnc/index.html
-
-# 2. تشغيل شاشة VNC داخلياً (ضفنا USER=root عشان يتأكد إنه واخد الصلاحيات)
-USER=root vncserver :1 -geometry 1280x720 -depth 24
+# 2. تشغيل شاشة VNC داخلياً
+vncserver :1 -geometry 1280x720 -depth 24
 
 # 3. تشغيل الجسر (noVNC) باستخدام websockify وتوجيهه للشبكة الخارجية (0.0.0.0:8080)
-# علامة & مهمة جداً عشان البوت يشتغل بعده في الخلفية
+# علامة & مهمة جداً عشان البوت يشتغل بعده
 websockify --web=/usr/share/novnc/ 0.0.0.0:8080 localhost:5901 &
 
-# 4. تشغيل بوت الميوزك (AnnieXBoda)
+# 4. تشغيل بوت الموزيك (AnnieXBoda)
 python3 run.py
