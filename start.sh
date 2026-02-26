9c085cbc73ef74d991366faf1a29bfb48c876aaa
b07849185-lang <b07849185@gmail.com>
2026-02-26 02:55:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,15 @@
 export USER=root
 export DISPLAY=:1
 
-# 1. تشغيل خادم الصوت
+# 1. تشغيل خادم الصوت (PulseAudio)
 pulseaudio -D --exit-idle-time=-1 --system
 
-# 2. تشغيل KasmVNC (بيعمل شاشة وهمية وبيشغل سيرفر الويب في نفس الوقت)
-# هنستخدم بورت 8080 عشان Fly.io يفتحه مباشر من الرابط بتاعك
-vncserver :1 -depth 24 -geometry 1280x720 -websocketPort 8080 -cert /etc/ssl/certs/ssl-cert-snakeoil.pem -key /etc/ssl/private/ssl-cert-snakeoil.key -Listen 0.0.0.0 &
+# 2. تشغيل KasmVNC مع تحديد مسار الباسورد وربطه بالبورت 8080 عشان يفتح من متصفحك مباشرة
+vncserver :1 -depth 24 -geometry 1280x720 -websocketPort 8080 -cert /etc/ssl/certs/ssl-cert-snakeoil.pem -key /etc/ssl/private/ssl-cert-snakeoil.key -Listen 0.0.0.0 -passwd /root/.vnc/passwd &
 sleep 3
 
-# 3. تشغيل واجهة XFCE (عشان تشوف التيرمينال والمتصفح)
+# 3. تشغيل واجهة الكمبيوتر (XFCE)
 startxfce4 &
 
-# 4. تشغيل بوت التليجرام
+# 4. تشغيل بوت التليجرام الخاص بك
 python3 run.py
