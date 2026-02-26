2efff469c39c9668364ab7fbc53c5900eaaeffc6
b07849185-lang <b07849185@gmail.com>
2026-02-26 07:39:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 export USER=root
-export HOME=/root
 
-# 1. حل مشكلة الكراش بتاعت الشهادة عشان السيرفر ميقفلش (مجربة ومضمونة)
-make-ssl-cert generate-default-snakeoil --force-overwrite
-chmod 644 /etc/ssl/private/ssl-cert-snakeoil.key
+# 1. مسح أي كاش قديم
+rm -rf /tmp/.X* /tmp/.x* /root/.vnc/*.log /root/.vnc/*.pid
 
-# 2. تشغيل الصوت (مهم عشان اليوتيوب والألعاب)
+# 2. تشغيل خادم الصوت كمسؤول (عشان يوتيوب والألعاب)
 pulseaudio -D --exit-idle-time=-1 --system
 
-# 3. إعداد KasmVNC لفتح البورت بدون SSL
-mkdir -p /root/.vnc
-cat <<EOF > /root/.vnc/kasmvnc.yaml
+# 3. إنشاء المستخدم kasmuser كما اقترحت بالضبط وإعطائه الصلاحيات
+useradd -m -s /bin/bash kasmuser
+echo "kasmuser:123456" | chpasswd
+usermod -aG sudo,audio,video kasmuser
+
+# 4. توليد شهادة SSL صامتة لتجنب أي كراش أمني
+make-ssl-cert generate-default-snakeoil --force-overwrite
+mkdir -p /home/kasmuser/.vnc
+cp /etc/ssl/private/ssl-cert-snakeoil.key /home/kasmuser/.vnc/cert.key
+cp /etc/ssl/certs/ssl-cert-snakeoil.pem /home/kasmuser/.vnc/cert.pem
+
+# 5. إنشاء إعدادات KasmVNC وتوجيهها للشهادات
+cat <<EOF > /home/kasmuser/.vnc/kasmvnc.yaml
 network:
   protocol: http
   interface: 0.0.0.0
   websocket_port: 8080
   ssl:
     require_ssl: false
+    pem_certificate: /home/kasmuser/.vnc/cert.pem
+    pem_key: /home/kasmuser/.vnc/cert.key
 EOF
 
-# 4. إعداد واجهة XFCE
-cat <<EOF > /root/.vnc/xstartup
+# 6. إعداد ملف واجهة XFCE
+cat <<EOF > /home/kasmuser/.vnc/xstartup
 #!/bin/bash
 startxfce4 &
 EOF
-chmod +x /root/.vnc/xstartup
+chmod +x /home/kasmuser/.vnc/xstartup
 
-# 5. تشغيل بوت التليجرام بتاعك في الخلفية 
+# 7. السر الحقيقي: تمرير حرف 'n' في نهاية الباسورد لرفض سؤال (View-Only Password) الخفي!
+su - kasmuser -c "mkdir -p ~/.vnc && echo -e '123456\n123456\nn\n' | kasmvncpasswd -u kasmuser -w"
+
+# 8. ضبط ملكية الملفات للمستخدم kasmuser
+chown -R kasmuser:kasmuser /home/kasmuser/.vnc
+
+# 9. تشغيل بوت التليجرام الخاص بيك في الخلفية 
 cd /app
 python3 run.py &
 
-# 6. الضربة القاضية: تشغيل KasmVNC مع إضافة الكود السري (-disableBasicAuth) لإلغاء الدوامة نهائياً!
-vncserver :1 -depth 24 -geometry 1280x720 -select-de xfce -disableBasicAuth -SecurityTypes None -fg
+# 10. تشغيل الواجهة بصلاحيات kasmuser
+su - kasmuser -c "vncserver :1 -depth 24 -geometry 1280x720 -select-de xfce -fg"
