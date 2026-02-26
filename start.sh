a3c515f8399fc69843fd41a0df6ecd3427e06831
b07849185-lang <b07849185@gmail.com>
2026-02-26 07:42:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 export USER=root
+export HOME=/root
 
-# 1. مسح أي كاش قديم
-rm -rf /tmp/.X* /tmp/.x* /root/.vnc/*.log /root/.vnc/*.pid
+# 1. تنظيف الكاش القديم
+rm -rf /tmp/.X* /tmp/.x* /root/.vnc/*.log /root/.vnc/*.pid /root/.vnc/passwd
 
-# 2. تشغيل خادم الصوت كمسؤول (عشان يوتيوب والألعاب)
+# 2. تشغيل خادم الصوت كمسؤول
 pulseaudio -D --exit-idle-time=-1 --system
 
-# 3. إنشاء المستخدم kasmuser كما اقترحت بالضبط وإعطائه الصلاحيات
-useradd -m -s /bin/bash kasmuser
-echo "kasmuser:123456" | chpasswd
-usermod -aG sudo,audio,video kasmuser
-
-# 4. توليد شهادة SSL صامتة لتجنب أي كراش أمني
+# 3. توليد شهادة SSL صامتة لتجنب أي كراش أمني
 make-ssl-cert generate-default-snakeoil --force-overwrite
-mkdir -p /home/kasmuser/.vnc
-cp /etc/ssl/private/ssl-cert-snakeoil.key /home/kasmuser/.vnc/cert.key
-cp /etc/ssl/certs/ssl-cert-snakeoil.pem /home/kasmuser/.vnc/cert.pem
 
-# 5. إنشاء إعدادات KasmVNC وتوجيهها للشهادات
-cat <<EOF > /home/kasmuser/.vnc/kasmvnc.yaml
+# 4. إنشاء إعدادات KasmVNC وتوجيهها للشهادات الافتراضية
+mkdir -p /root/.vnc
+cat <<EOF > /root/.vnc/kasmvnc.yaml
 network:
   protocol: http
   interface: 0.0.0.0
   websocket_port: 8080
   ssl:
     require_ssl: false
-    pem_certificate: /home/kasmuser/.vnc/cert.pem
-    pem_key: /home/kasmuser/.vnc/cert.key
 EOF
 
-# 6. إعداد ملف واجهة XFCE
-cat <<EOF > /home/kasmuser/.vnc/xstartup
+# 5. إعداد ملف واجهة XFCE
+cat <<EOF > /root/.vnc/xstartup
 #!/bin/bash
 startxfce4 &
 EOF
-chmod +x /home/kasmuser/.vnc/xstartup
+chmod +x /root/.vnc/xstartup
 
-# 7. السر الحقيقي: تمرير حرف 'n' في نهاية الباسورد لرفض سؤال (View-Only Password) الخفي!
-su - kasmuser -c "mkdir -p ~/.vnc && echo -e '123456\n123456\nn\n' | kasmvncpasswd -u kasmuser -w"
+# 6. السر الحقيقي: عمل الباسورد بطريقة مباشرة للمستخدم root وإجباره يتجاهل الويزارد
+echo "123456" | kasmvncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
 
-# 8. ضبط ملكية الملفات للمستخدم kasmuser
-chown -R kasmuser:kasmuser /home/kasmuser/.vnc
-
-# 9. تشغيل بوت التليجرام الخاص بيك في الخلفية 
+# 7. تشغيل بوت التليجرام الخاص بك في الخلفية
 cd /app
 python3 run.py &
 
-# 10. تشغيل الواجهة بصلاحيات kasmuser
-su - kasmuser -c "vncserver :1 -depth 24 -geometry 1280x720 -select-de xfce -fg"
+# 8. تشغيل الواجهة بصلاحيات root مع تحديد ملف الباسورد اللي عملناه وتخطي الحماية
+vncserver :1 -depth 24 -geometry 1280x720 -select-de xfce -PasswordFile /root/.vnc/passwd -disableBasicAuth -fg
