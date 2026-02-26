ad453d187433237c4444b0d905fff4be9daea326
b07849185-lang <b07849185@gmail.com>
2026-02-26 08:04:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 export USER=root
 export HOME=/root
+export DISPLAY=:1
 
-# 1. تنظيف السيرفر من أي كاش قديم
+# 1. مسح الكاش
 rm -rf /tmp/.X* /tmp/.x* /root/.vnc/*.log /root/.vnc/*.pid
 
-# 2. تشغيل خادم الصوت (عشان اليوتيوب اللي كان بيقطع)
+# 2. تشغيل الصوت
 pulseaudio -D --exit-idle-time=-1 --system
 
-# 3. توليد شهادة SSL الوهمية عشان السيرفر ميقفلش
-make-ssl-cert generate-default-snakeoil --force-overwrite
-chmod 644 /etc/ssl/private/ssl-cert-snakeoil.key
-
-# 4. إعداد KasmVNC
+# 3. إعداد باسورد الـ VNC
 mkdir -p /root/.vnc
-cat <<EOF > /root/.vnc/kasmvnc.yaml
-network:
-  protocol: http
-  interface: 0.0.0.0
-  websocket_port: 8080
-  ssl:
-    require_ssl: false
-EOF
+echo "123456" | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
 
-# 5. التعديل السحري: إضافة dbus-launch لمنع سطح المكتب من الانهيار (Crash)
+# 4. إعداد ملف xstartup
 cat <<EOF > /root/.vnc/xstartup
-#!/bin/bash
-unset SESSION_MANAGER
-unset DBUS_SESSION_BUS_ADDRESS
-export XDG_CURRENT_DESKTOP="XFCE"
-exec dbus-launch --exit-with-session startxfce4 &
+#!/bin/sh
+xrdb $HOME/.Xclients
+xsetroot -solid grey
+export XKL_XMODMAP_DISABLE=1
+/etc/X11/Xsession
+startxfce4 &
 EOF
 chmod +x /root/.vnc/xstartup
 
-# 6. تشغيل بوت التليجرام الخاص بك في الخلفية
-cd /app
-python3 run.py &
+# 5. تشغيل سيرفر VNC
+tightvncserver :1 -geometry 1280x720 -depth 24
 
-# 7. تشغيل KasmVNC بدون الدوامة (بإرسال رقم 2 تلقائياً)
-echo "2" | vncserver :1 -depth 24 -geometry 1280x720 -select-de xfce -SecurityTypes None -disableBasicAuth -fg
+# 6. تشغيل noVNC (يربط بورت 8080 للعامة بالـ VNC الداخلي)
+websockify --web /usr/share/novnc/ 0.0.0.0:8080 127.0.0.1:5901 &
+
+# 7. تشغيل البوت
+python3 run.py
